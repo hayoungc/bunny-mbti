1a9d6907c31e393429e41fa79c7b9be6048d798a
hayoungc <hayoungcau@gmail.com>
2023-06-17 02:36:35 -0400

    title

--- src/components/Metatag.tsx
+++ src/components/Metatag.tsx
@@ -8,7 +8,7 @@ interface MetatagProps {
 }
 
 function Metatag({
-  title = 'My bunny type test',
+  title = 'My bunny MBTI test',
   description = '나는 어떤 유형의 버니일까?',
   image = 'https://rawcdn.githack.com/borachoidev/My-Idol-Type/9db39f52240ecc175551323967d908cdc78fd6f0/public/images/5.jpg?raw=true',
 }: MetatagProps) {
@@ -17,9 +17,9 @@ function Metatag({
       <title>{title} | MBTI 테스트 </title>
       <meta name="description" content={description} />
       <meta name="keywords" content="mbti,MBTITest,mbti테스트" />
-      <meta property="og:title" content={title + '| My Bunny Type Test'} />
+      <meta property="og:title" content={title + '| 나의 MBTI를 찾아서'} />
       <meta property="og:type" content="article" />
-      <meta property="og:site_name" content="My Bunny Type Test" />
+      <meta property="og:site_name" content="나의 MBTI를 찾아서" />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
       <meta name="twitter:title" content={title} />
